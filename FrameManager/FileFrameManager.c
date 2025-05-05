4971aa3f450971f6eae31bf2f9e7b0ff9c63c8d7
pedrit <pedroupnainformatica@gmail.com>
2025-05-05 13:40:29 +0200

    [40] - Writte RVRB frame

--- FrameManager/FileFrameManager.c
+++ FrameManager/FileFrameManager.c
@@ -116,8 +116,19 @@ void FileManager_writteUSERInFile(FILE *destFilePtr, ID3v2USERFrameType USER){
     fwrite(&USER.language,1, 3,destFilePtr);
     fwrite(USER.actualText.string,1, TxtStr_getStringLen(USER.actualText),destFilePtr);
 }
-//USER
-//RVRB
+void FileManager_writteRVRBInFile(FILE *destFilePtr, ID3v2RVRBFrameType RVRB){
+    fwrite(&RVRB.header,1, sizeof(RVRB.header),destFilePtr);
+    fwrite(&RVRB.left,1,sizeof(uint16_t),destFilePtr);
+    fwrite(&RVRB.right,1,sizeof(uint16_t),destFilePtr);
+    fwrite(&RVRB.bouncesL,1,sizeof(uint8_t),destFilePtr);
+    fwrite(&RVRB.bouncesR,1,sizeof(uint8_t),destFilePtr);
+    fwrite(&RVRB.feedBackLL,1,sizeof(uint8_t),destFilePtr);
+    fwrite(&RVRB.feedBackLLR,1,sizeof(uint8_t),destFilePtr);
+    fwrite(&RVRB.feedBackRR,1,sizeof(uint8_t),destFilePtr);
+    fwrite(&RVRB.feedBackRL,1,sizeof(uint8_t),destFilePtr);
+    fwrite(&RVRB.premixLR,1,sizeof(uint8_t),destFilePtr);
+    fwrite(&RVRB.premixRL,1,sizeof(uint8_t),destFilePtr);
+}
 ////////////////////////////
 
 void FileManager_writtePadding(FILE *destFilePtr, int paddingSize){
@@ -210,11 +221,10 @@ void FileManager_writteTagIntoFile(char *file, ID3TagType *ID3Tag){
         if(ID3Tag->IPLS != NULL) FileManager_writteIPLSInFile(temp,*ID3Tag->IPLS);
         if(ID3Tag->SYTC != NULL) FileManager_writteDefaultFrameInFile(temp,*ID3Tag->SYTC);
         if(ID3Tag->USER != NULL) FileManager_writteUSERInFile(temp,*ID3Tag->USER);
-        //
         if(ID3Tag->OWNE != NULL) FileManager_writteDefaultFrameInFile(temp,*ID3Tag->OWNE);
         if(ID3Tag->COMR != NULL) FileManager_writteDefaultFrameInFile(temp,*ID3Tag->COMR);
         if(ID3Tag->POSS != NULL) FileManager_writteDefaultFrameInFile(temp,*ID3Tag->POSS);
-        //RVRB
+        if(ID3Tag->RVRB != NULL) FileManager_writteRVRBInFile(temp,*ID3Tag->RVRB);
         if(ID3Tag->EQUA != NULL) FileManager_writteDefaultFrameInFile(temp,*ID3Tag->EQUA);
         if(ID3Tag->MLLT != NULL) FileManager_writteDefaultFrameInFile(temp,*ID3Tag->MLLT);
         if(ID3Tag->ETCO != NULL) FileManager_writteDefaultFrameInFile(temp,*ID3Tag->ETCO);
